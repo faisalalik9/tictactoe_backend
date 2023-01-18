5dc83dcf0bbf374c238426235b331024ccbfd4f5
Faisal <faisalalik9@gmail.com>
2023-01-18 23:07:05 +0530

    Refactoring

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ import cookieParser from "cookie-parser";
  app.use(express.json());
  app.use(express.urlencoded({extended:true}));
  app.use(cookieParser());
- import authRouter from './router/authRouter.js';
- import userRouter from "./router/userRouter.js";
-import gamesRouter from "./router/gamesRouter.js";
+ import authRouter from './src/router/authRouter.js';
+ import userRouter from "./src/router/userRouter.js";
+import gamesRouter from "./src/router/gamesRouter.js";
 
   app.use('/authenticate',authRouter);
   app.use('/users',userRouter);
