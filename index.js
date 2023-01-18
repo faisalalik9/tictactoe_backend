c5b3a81c0f9ecc46f02534eef173363a23510e0d
Faisal <faisalalik9@gmail.com>
2023-01-18 23:12:23 +0530

    Minor changes

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
- import express from "express";
- import cors from "cors";
+import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
  
 
- const app = express();
+const app = express();
 
 
- app.use(cors());
- app.use(express.json());
- app.use(express.urlencoded({extended:true}));
- app.use(cookieParser());
- import authRouter from './src/router/authRouter.js';
- import userRouter from "./src/router/userRouter.js";
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+import authRouter from './src/router/authRouter.js';
+import userRouter from "./src/router/userRouter.js";
 import gamesRouter from "./src/router/gamesRouter.js";
 
-  app.use('/authenticate',authRouter);
-  app.use('/users',userRouter);
-  app.use('/games',gamesRouter);
-  app.use('/',(req,res)=>{res.json("Server ruinng!")})
+app.use('/authenticate',authRouter);
+app.use('/users',userRouter);
+app.use('/games',gamesRouter);
+app.use('/',(req,res)=>{res.json("Server ruinng!")})
  
- 
- const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
 
- app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`);
- })
+app.listen(PORT, ()=>{
+   console.log(`Server is running on ${PORT}`);
+});
